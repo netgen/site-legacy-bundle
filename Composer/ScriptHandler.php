f789068ad3ec5322a38ac72289ac5d918e7844a0
Edi Modrić <edi.modric@gmail.com>
2015-02-24 14:44:04 +0100

    Add composer script handlers for symlinking legacy files and running legacy autoload generator

---
+++ Composer/ScriptHandler.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Netgen\Bundle\MoreLegacyBundle\Composer;
+
+use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler as DistributionBundleScriptHandler;
+use Composer\Script\CommandEvent;
+use Symfony\Component\Process\ProcessBuilder;
+
+class ScriptHandler extends DistributionBundleScriptHandler
+{
+    /**
+     * Symlinks legacy siteaccesses and various other legacy files to their proper locations
+     *
+     * @param $event \Composer\Script\CommandEvent
+     */
+    public static function installLegacySymlinks( CommandEvent $event )
+    {
+        $options = self::getOptions( $event );
+        $appDir = $options['symfony-app-dir'];
+
+        if ( !is_dir( $appDir ) )
+        {
+            echo 'The symfony-app-dir (' . $appDir . ') specified in composer.json was not found in ' . getcwd() . ', can not install legacy symlinks.' . PHP_EOL;
+            return;
+        }
+
+        static::executeCommand( $event, $appDir, 'ngmore:symlink:legacy', $options['process-timeout'] );
+    }
+
+    /**
+     * Generates legacy autoloads
+     *
+     * @param $event \Composer\Script\CommandEvent
+     */
+    public static function generateLegacyAutoloads( CommandEvent $event )
+    {
+        $options = self::getOptions( $event );
+
+        $currentWorkingDirectory = getcwd();
+        $legacyRootDir = $currentWorkingDirectory . '/' . $options['ezpublish-legacy-dir'];
+
+        if ( !is_dir( $legacyRootDir ) )
+        {
+            echo 'The ezpublish-legacy-dir (' . $options['ezpublish-legacy-dir'] . ') specified in composer.json was not found in ' . $currentWorkingDirectory . ', can not generate legacy autoloads.' . PHP_EOL;
+            return;
+        }
+
+        chdir( $legacyRootDir );
+
+        $processBuilder = new ProcessBuilder(
+            array(
+                'php',
+                'bin/php/ezpgenerateautoloads.php'
+            )
+        );
+
+        $process = $processBuilder->getProcess();
+
+        $process->setTimeout( 3600 );
+        $process->run(
+            function ( $type, $buffer )
+            {
+                echo $buffer;
+            }
+        );
+
+        chdir( $currentWorkingDirectory );
+    }
+}
