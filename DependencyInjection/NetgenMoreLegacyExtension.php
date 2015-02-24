a3e20a00d23776d78c4ea368d037ea4746dbfd1a
Edi Modrić <edi.modric@gmail.com>
2015-02-24 15:00:29 +0100

    Add legacy config mapper

--- DependencyInjection/NetgenMoreLegacyExtension.php
+++ DependencyInjection/NetgenMoreLegacyExtension.php
@@ -24,6 +24,7 @@ class NetgenMoreLegacyExtension extends Extension
 
         $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
 
+        $loader->load( 'parameters.yml' );
         $loader->load( 'services.yml' );
     }
 }
