d2c616198c833d4cfda9a8f63722316354e6d507
Edi Modrić <edi.modric@gmail.com>
2016-04-26 09:45:47 +0200

    Update typehint in composer script handler to use event class

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -3,16 +3,16 @@
 namespace Netgen\Bundle\MoreLegacyBundle\Composer;
 
 use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler as DistributionBundleScriptHandler;
-use Composer\Script\CommandEvent;
+use Composer\Script\Event;
 
 class ScriptHandler extends DistributionBundleScriptHandler
 {
     /**
      * Symlinks legacy siteaccesses and various other legacy files to their proper locations.
      *
-     * @param $event \Composer\Script\CommandEvent
+     * @param $event \Composer\Script\Event
      */
-    public static function installLegacySymlinks(CommandEvent $event)
+    public static function installLegacySymlinks(Event $event)
     {
         $options = self::getOptions($event);
         $appDir = $options['symfony-app-dir'];
