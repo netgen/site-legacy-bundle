f13121a523560a408f835896c63c89d7ec1b6f36
Edi Modrić <edi.modric@gmail.com>
2017-04-27 21:19:56 +0200

    Implement compiler pass to override eZ legacy config mapper

--- NetgenMoreLegacyBundle.php
+++ NetgenMoreLegacyBundle.php
@@ -3,7 +3,18 @@
 namespace Netgen\Bundle\MoreLegacyBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Netgen\Bundle\MoreLegacyBundle\DependencyInjection\Compiler;
 
 class NetgenMoreLegacyBundle extends Bundle
 {
+    /**
+     * Builds the bundle.
+     *
+     * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new Compiler\ConfigurationMapperPass());
+    }
 }
