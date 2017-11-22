4efa88d7bbb9d8ddfa23ceaca0d95b2e2b967e77
Edi Modrić <edi.modric@gmail.com>
2017-11-22 13:23:35 +0100

    Use getConsoleDir method in composer script handler

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -14,15 +14,9 @@ class ScriptHandler extends DistributionBundleScriptHandler
      */
     public static function installLegacySymlinks(Event $event)
     {
-        $options = self::getOptions($event);
-        $binDir = $options['symfony-bin-dir'];
+        $options = static::getOptions($event);
+        $consoleDir = static::getConsoleDir($event, 'install legacy symlinks');
 
-        if (!is_dir($binDir)) {
-            echo 'The symfony-bin-dir (' . $binDir . ') specified in composer.json was not found in ' . getcwd() . ', can not install legacy symlinks.' . PHP_EOL;
-
-            return;
-        }
-
-        static::executeCommand($event, $binDir, 'ngmore:symlink:legacy', $options['process-timeout']);
+        static::executeCommand($event, $consoleDir, 'ngmore:symlink:legacy', $options['process-timeout']);
     }
 }
