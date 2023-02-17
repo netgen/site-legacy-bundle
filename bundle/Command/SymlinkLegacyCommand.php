030172622e7406563cbca94bf87cb4f09aadcc67
Edi Modrić <edi.modric@gmail.com>
2023-02-17 11:03:53 +0100

    Do not use removed base class from site-bundle

--- bundle/Command/SymlinkLegacyCommand.php
+++ bundle/Command/SymlinkLegacyCommand.php
@@ -5,16 +5,19 @@ declare(strict_types=1);
 namespace Netgen\Bundle\SiteLegacyBundle\Command;
 
 use DirectoryIterator;
-use Netgen\Bundle\SiteBundle\Command\SymlinkCommand;
 use Netgen\Bundle\SiteBundle\NetgenSiteProjectBundleInterface;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\KernelInterface;
+
+use function basename;
 use function date;
+use function dirname;
 use function file_get_contents;
 use function in_array;
 use function is_dir;
@@ -28,6 +31,11 @@ use function strrev;
 
 class SymlinkLegacyCommand extends SymlinkCommand
 {
+    /**
+     * If true, command will destroy existing symlinks before recreating them.
+     */
+    protected bool $forceSymlinks = false;
+
     /**
      * The list of folders available in standard distribution of eZ Publish Legacy.
      */
@@ -61,6 +69,8 @@ class SymlinkLegacyCommand extends SymlinkCommand
     ];
 
     protected KernelInterface $kernel;
+
+    protected Filesystem $fileSystem;
 
     protected string $legacyRootDir;
 
@@ -281,5 +291,83 @@ class SymlinkLegacyCommand extends SymlinkCommand
                 }
             }
         }
+    }
+
+    /**
+     * Verify that source file can be symlinked to destination and do symlinking if it can.
+     */
+    protected function verifyAndSymlinkFile(string $source, string $destination, OutputInterface $output): void
+    {
+        if (!$this->fileSystem->exists(dirname($destination))) {
+            $this->fileSystem->mkdir(dirname($destination), 0755);
+        }
+
+        if ($this->fileSystem->exists($destination) && !is_file($destination)) {
+            $output->writeln('<comment>' . basename($destination) . '</comment> already exists in <comment>' . dirname($destination) . '/</comment> and is not a file/symlink. Skipping...');
+
+            return;
+        }
+
+        if ($this->forceSymlinks && is_link($destination)) {
+            $this->fileSystem->remove($destination);
+        }
+
+        if (is_file($destination) && !is_link($destination)) {
+            if ($this->fileSystem->exists($destination . '.original')) {
+                $output->writeln('Cannot create backup file <comment>' . basename($destination) . '.original</comment> in <comment>' . dirname($destination) . '/</comment>. Skipping...');
+
+                return;
+            }
+
+            $this->fileSystem->rename($destination, $destination . '.original');
+        }
+
+        if ($this->fileSystem->exists($destination)) {
+            if (is_link($destination)) {
+                $output->writeln('Skipped creating the symlink for <comment>' . basename($destination) . '</comment> in <comment>' . dirname($destination) . '/</comment>. Symlink already exists!');
+            } else {
+                $output->writeln('Skipped creating the symlink for <comment>' . basename($destination) . '</comment> in <comment>' . dirname($destination) . '/</comment> due to an unknown error.');
+            }
+
+            return;
+        }
+
+        $this->fileSystem->symlink(
+            $this->fileSystem->makePathRelative(
+                dirname($source),
+                realpath(dirname($destination)),
+            ) . basename($source),
+            $destination,
+        );
+    }
+
+    /**
+     * Verify that source directory can be symlinked to destination and do symlinking if it can.
+     */
+    protected function verifyAndSymlinkDirectory(string $source, string $destination, OutputInterface $output): void
+    {
+        if ($this->fileSystem->exists($destination) && !is_link($destination)) {
+            $output->writeln('<comment>' . basename($destination) . '</comment> already exists in <comment>' . dirname($destination) . '/</comment> and is not a symlink. Skipping...');
+
+            return;
+        }
+
+        if ($this->forceSymlinks && is_link($destination)) {
+            $this->fileSystem->remove($destination);
+        }
+
+        if ($this->fileSystem->exists($destination)) {
+            $output->writeln('Skipped creating the symlink for <comment>' . basename($destination) . '</comment> in <comment>' . dirname($destination) . '/</comment>. Symlink already exists!');
+
+            return;
+        }
+
+        $this->fileSystem->symlink(
+            $this->fileSystem->makePathRelative(
+                $source,
+                realpath(dirname($destination)),
+            ),
+            $destination,
+        );
     }
 }
