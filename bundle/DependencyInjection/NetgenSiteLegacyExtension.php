fa82997478c4cbdd9e1fa00419ba332d9d22cf77
Edi Modrić <edi.modric@gmail.com>
2023-02-22 15:55:53 +0100

    Add basic edit and view templates for novaseometas data type

--- bundle/DependencyInjection/NetgenSiteLegacyExtension.php
+++ bundle/DependencyInjection/NetgenSiteLegacyExtension.php
@@ -7,11 +7,14 @@ namespace Netgen\Bundle\SiteLegacyBundle\DependencyInjection;
 use Ibexa\Bundle\Core\DependencyInjection\Configuration\SiteAccessAware\ConfigurationProcessor;
 use Ibexa\Bundle\Core\DependencyInjection\Configuration\SiteAccessAware\ContextualizerInterface;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Yaml\Yaml;
 
-class NetgenSiteLegacyExtension extends Extension
+class NetgenSiteLegacyExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
@@ -31,5 +34,19 @@ class NetgenSiteLegacyExtension extends Extension
 
         $processor->mapConfigArray('injected_settings', $config, ContextualizerInterface::MERGE_FROM_SECOND_LEVEL);
         $processor->mapConfigArray('injected_merge_settings', $config, ContextualizerInterface::MERGE_FROM_SECOND_LEVEL);
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $prependConfigs = [
+            'admin/novaseometas.yaml' => 'ibexa',
+        ];
+
+        foreach ($prependConfigs as $configFile => $prependConfig) {
+            $configFile = __DIR__ . '/../Resources/config/' . $configFile;
+            $config = Yaml::parse(file_get_contents($configFile));
+            $container->prependExtensionConfig($prependConfig, $config);
+            $container->addResource(new FileResource($configFile));
+        }
     }
 }
---
+++ bundle/ezpublish_legacy/ngsite/design/standard/templates/content/datatype/view/novaseometas.tpl
@@ -0,0 +1,7 @@
+{symfony_include(
+    '@NetgenSiteLegacy/field_type/novaseometas/field/view.html.twig',
+    hash(
+        'content', $attribute.object,
+        'field_name', $attribute.contentclass_attribute.identifier
+    )
+)}
